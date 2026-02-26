ddd049b307f0af2a4cc269073e4dee4534eab151
oaf1 <113090927+oaf1@users.noreply.github.com>
2026-02-26 11:10:48 +0100

    jelszo
    8 kar, kisbetu, nagybetu, szam

--- backend/src/controllers/register.controller.js
+++ backend/src/controllers/register.controller.js
@@ -11,6 +11,14 @@ exports.register = async (req, res) => {
     });
   }
 
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+if (!passwordRegex.test(password)) {
+  return res.status(400).json({
+    message: "A jelszónak legalább 8 karakter hosszúnak kell lennie, és tartalmaznia kell kisbetűt, nagybetűt és számot."
+  });
+}
+
   try {
     // Jelszó hash
     const hashedPassword = await bcrypt.hash(password, 10);
